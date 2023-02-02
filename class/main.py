01995d3eca076b802ba1017f18353fb37361ba33
Suryanalupura <124047307+Suryanalupura@users.noreply.github.com>
2023-02-02 14:40:28 +0530

    Update main.py

--- class/main.py
+++ class/main.py
@@ -1,7 +1,6 @@
 import Student
 
-student = Student("Akash R Chandran","Kollam",25)
-student.display_student_details()
-print("="*20)
-student.set_age(21)
-student.display_student_details()
+s = Student("Akash R Chandran","Kollam",25)
+s.display_student_details()
+s.set_age(21)
+s.display_student_details()
