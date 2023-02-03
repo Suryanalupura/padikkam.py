42eba4491605c39c2fb8852db88cb9c61d08f559
Suryanalupura <124047307+Suryanalupura@users.noreply.github.com>
2023-02-03 21:37:52 +0530

    Update compress.py

--- Image_compresser/compress.py
+++ Image_compresser/compress.py
@@ -6,7 +6,9 @@ print("IMAGE COMPRESSOR")
 print("UPLOAD FILE")
 file_path = askopenfilename()
 img = PIL.Image.open(file_path)
-myHeight , myWidth = img.size
-img = img.resize((myHeight,myWidth) , PIL.Image.ANTIALIAS)
+#myHeight , myWidth = img.size
+#size can be adjusted or set to default as the original image size
+#here size is set at 50x50
+img = img.resize((50,50) , PIL.Image.ANTIALIAS)
 save_path = asksaveasfilename()
 img.save(save_path+"_compressed.JPG")
